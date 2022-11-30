802f8b2acef52ae9848342efb39f4a1485d3a31c
Anatoly <gusev.tolia@yandex.ru>
2022-11-30 04:09:09 +0000

    Add clickhouse-client feature

---
+++ src/clickhouse-client/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Downloading psql..."
+
+check_packages apt-transport-https ca-certificates dirmngr
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8919F6BD2B48D754
+echo "deb https://packages.clickhouse.com/deb stable main" | sudo tee /etc/apt/sources.list.d/clickhouse.list
+apt-get update -y
+apt-get install -y clickhouse-client
