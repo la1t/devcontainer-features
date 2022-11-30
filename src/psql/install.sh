8599d608646863d90c8f748e47c71ce01e5adf48
Anatoly <gusev.tolia@yandex.ru>
2022-11-30 03:59:07 +0000

    Add psql feature

---
+++ src/psql/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Downloading psql..."
+
+apt_get_update
+apt-get -y install postgresql-client
